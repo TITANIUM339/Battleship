042d2846fbf8b778349669c84715bf6132a77667
TITANIUM339 <65349454+TITANIUM339@users.noreply.github.com>
2024-03-18 13:44:41 +0300

    Fix computer play algorithm and add a time delay

--- src/modules/computer.js
+++ src/modules/computer.js
@@ -1,6 +1,7 @@
 import { Player } from "./player";
 
 class Computer {
+    // A reference board to keep track of valid slots.
     #board = Array.from(Array(10), () => new Array(10).fill(true));
     #destroyingShip = false;
     #hits = [];
@@ -14,11 +15,11 @@ class Computer {
             throw new Error("player must be an instance of Player");
 
         this.#player = player;
-        this.#checkerboard = this.#calculateCheckerBoard();
+        this.#checkerboard = this.#generateCheckerBoard();
     }
 
-    #calculateCheckerBoard() {
-        const ships = this.#player.enemyUnSunkShips();
+    #generateCheckerBoard() {
+        const ships = this.#player.enemyUnSunkShips;
 
         let largestShipLength = 1;
 
@@ -86,30 +87,31 @@ class Computer {
 
     #attack(coordinate) {
         const [x, y] = coordinate;
-        const enemyShipsBeforeAttack = this.#player.enemyUnSunkShips();
         const result = this.#player.attack(coordinate);
-        const enemyShipsAfterAttack = this.#player.enemyUnSunkShips();
 
         if (result) this.#board[x][y] = false;
 
         switch (result) {
             case 3: {
-                let sunkShipLength;
+                const sunkShips = this.#player.enemySunkShips;
+                const sunkShipCoordinates = sunkShips[sunkShips.length - 1][1];
 
-                for (let i = 0; i < enemyShipsBeforeAttack.length; i++) {
-                    if (
-                        i === enemyShipsAfterAttack.length ||
-                        enemyShipsBeforeAttack[i][0] !==
-                            enemyShipsAfterAttack[i][0]
-                    ) {
-                        sunkShipLength = enemyShipsBeforeAttack[i][1];
+                // Remove sunk ship coordinates from hits array.
+                for (let i = 0; i < sunkShipCoordinates.length; i++) {
+                    const [x1, y1] = sunkShipCoordinates[i];
+
+                    for (let j = 0; j < this.#hits.length; j++) {
+                        const [x2, y2] = this.#hits[j];
+
+                        if (x1 === x2 && y1 === y2) {
+                            this.#hits.splice(j, 1);
+                            break;
+                        }
                     }
                 }
 
-                const spliceValue = this.#hits.length - sunkShipLength + 1;
-                this.#hits.splice(spliceValue);
-
-                this.#checkerboard = this.#calculateCheckerBoard();
+                // Adjust the checkerboard pattern to account for the sunk ship.
+                this.#checkerboard = this.#generateCheckerBoard();
 
                 this.#destroyingShip = false;
                 this.#firstHit = this.#secondHit = null;
@@ -123,52 +125,77 @@ class Computer {
     }
 
     play(coordinate = this.#getRandomMove()) {
-        // Continue destroying ship.
-        if (this.#destroyingShip) {
-            const [x1, y1] = this.#firstHit;
-            const [x2, y2] = this.#secondHit;
+        return new Promise((resolve) =>
+            setTimeout(() => {
+                (() => {
+                    // Continue destroying ship.
+                    if (this.#destroyingShip) {
+                        const [x1, y1] = this.#firstHit;
+                        const [x2, y2] = this.#secondHit;
 
-            let coordinate = x1 === x2 ? [x2, y2 + 1] : [x2 + 1, y2];
-            let result = this.#attack(coordinate);
+                        // Second hit right/up.
+                        let coordinate =
+                            x1 === x2 ? [x2, y2 + 1] : [x2 + 1, y2];
+                        let result = this.#attack(coordinate);
 
-            if (result > 1) this.#secondHit = coordinate;
+                        if (result === 2) this.#secondHit = coordinate;
 
-            if (result) return;
+                        if (result) return;
 
-            coordinate = x1 === x2 ? [x1, y1 - 1] : [x1 - 1, y1];
-            result = this.#attack(coordinate);
+                        // Second hit left/down.
+                        coordinate = x1 === x2 ? [x2, y2 - 1] : [x2 - 1, y2];
+                        result = this.#attack(coordinate);
 
-            if (result > 1) this.#firstHit = coordinate;
+                        if (result === 2) this.#secondHit = coordinate;
 
-            if (result) return;
+                        if (result) return;
 
-            this.#destroyingShip = false;
-            this.#firstHit = this.#secondHit = null;
-        }
+                        // First hit right/up.
+                        coordinate = x1 === x2 ? [x1, y1 + 1] : [x1 + 1, y1];
+                        result = this.#attack(coordinate);
 
-        // Make a hit around a coordinate in the hits array.
-        if (this.#hits.length) {
-            if (this.#firstHit === null) {
-                this.#firstHit = this.#hits[this.#hits.length - 1];
-            }
+                        if (result === 2) this.#firstHit = coordinate;
 
-            const coordinate = this.#getRandomMoveAround(this.#firstHit);
-            const result = this.#attack(coordinate);
+                        if (result) return;
 
-            if (result === 2) {
-                this.#secondHit = this.#hits[this.#hits.length - 1];
-                this.#destroyingShip = true;
-            }
+                        // First hit left/down.
+                        coordinate = x1 === x2 ? [x1, y1 - 1] : [x1 - 1, y1];
+                        result = this.#attack(coordinate);
 
-            return;
-        }
+                        if (result === 2) this.#firstHit = coordinate;
 
-        // Make random move.
-        const result = this.#attack(coordinate);
+                        if (result) return;
 
-        if (result === 2) {
-            this.#firstHit = this.#hits[this.#hits.length - 1];
-        }
+                        this.#destroyingShip = false;
+                        this.#firstHit = this.#secondHit = null;
+                    }
+
+                    // Make a hit around a coordinate in the hits array.
+                    if (this.#hits.length) {
+                        if (this.#firstHit === null) {
+                            this.#firstHit = this.#hits[this.#hits.length - 1];
+                        }
+
+                        const coordinate = this.#getRandomMoveAround(
+                            this.#firstHit,
+                        );
+                        const result = this.#attack(coordinate);
+
+                        if (result === 2) {
+                            this.#secondHit = this.#hits[this.#hits.length - 1];
+                            this.#destroyingShip = true;
+                        }
+
+                        return;
+                    }
+
+                    // Make random move.
+                    this.#attack(coordinate);
+                })();
+
+                resolve();
+            }, 600),
+        );
     }
 }
 
